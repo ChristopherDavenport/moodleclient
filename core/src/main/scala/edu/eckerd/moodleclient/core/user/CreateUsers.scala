53d580d8af5389b6d08185e5cc9f4225250bd835
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-05-11 17:45:27 -0400

    Successfully Create a User

---
+++ core/src/main/scala/edu/eckerd/moodleclient/core/user/CreateUsers.scala
@@ -0,0 +1,51 @@
+package edu.eckerd.moodleclient.core.user
+
+import edu.eckerd.moodleclient.MoodleAble
+import edu.eckerd.moodleclient.models.{User, Users}
+import io.circe.Json
+import org.http4s.UrlForm
+
+case class CreateUsers(users: List[User])
+
+object CreateUsers {
+  implicit val CreateUsersMoodleAble = new MoodleAble[CreateUsers, Json]{
+
+    def render(input: CreateUsers): UrlForm = {
+      val functionName = List(("wsfunction", "core_user_create_users"))
+      val head = input.users.head
+      val refined = List(
+        ("users[0][username]", head.username), //", ""
+      ("users[0][password]", "Password123#"),  //= string
+      ("users[0][createpassword]", "0"), //= int
+      ("users[0][firstname]", head.firstname), //= string
+      ("users[0][lastname]", head.lastname), //= string
+      ("users[0][email]", head.email), //= string
+      ("users[0][auth]", head.auth), //= string
+      ("users[0][idnumber]", head.idnumber.getOrElse("")) //= string
+//      ("users[0][lang]", head.lang.getOrElse("en")), //= string
+//      ("users[0][calendartype]", head.calendartype.getOrElse("gregorian")), //= string
+//      ("users[0][theme]", ""), //= string
+//      ("users[0][timezone]", ""), //= string
+//      ("users[0][mailformat]", ""), //= int
+//      ("users[0][description]", ""), //= string
+//      ("users[0][city]", head.city.getOrElse("St Petersburg")), //= string
+//      ("users[0][country]", head.country.getOrElse("US")) //= string
+//      ("users[0][firstnamephonetic]", ""), //= string
+//      ("users[0][lastnamephonetic]", ""), //= string
+//      ("users[0][middlename]", ""), //= string
+//      ("users[0][alternatename]", ""), //= string
+//      ("users[0][preferences][0][type]", ""), //= string
+//      ("users[0][preferences][0][value]", ""), //= string
+//      ("users[0][customfields][0][type]", ""), //= string
+//      ("users[0][customfields][0][value]", "") //= string
+      )
+
+      UrlForm(
+        (functionName ++ refined):_*
+//        functionName ++ refined:_*
+      )
+    }
+  }
+
+
+}
--- core/src/main/scala/edu/eckerd/moodleclient/models/User.scala
+++ core/src/main/scala/edu/eckerd/moodleclient/models/User.scala
@@ -3,40 +3,54 @@ package edu.eckerd.moodleclient.models
 import io.circe.generic.JsonCodec
 
 @JsonCodec case class User(
+                            username: String ,
+                            firstname: String,
+                            lastname: String,
+                            email: String,
+
+                            auth : String = "Manual", //Auth plugins include manual, ldap, imap, etc
                             id: Option[Int] = None,
-                            username: Option[String] = None,
-                            firstname: Option[String] = None,
-                            lastname: Option[String] = None,
-                            fullname: Option[String],
-                            email: Option[String] = None,
+                            fullname: Option[String] = None,
+                            idnumber : Option[String] = None, //An arbitrary ID code number perhaps from the institution
+
                             address: Option[String] = None, //Postal address
                             phone1: Option[String] = None, //Phone 1
                             phone2: Option[String] = None, //Phone 2
+
                             icq : Option[String] = None, //icq number
                             skype : Option[String] = None, //skype id
                             yahoo : Option[String] = None, //yahoo id
                             aim : Option[String] = None,  //aim id
                             msn:  Option[String] = None,  //msn number
+
                             department : Option[String] = None, //department
                             institution : Option[String] = None, //institution
-                            idnumber : Option[String] = None, //An arbitrary ID code number perhaps from the institution
+
                             interests : Option[String] = None, //user interests (separated by commas)
-                            firstaccess : Option[BigDecimal] = None, //first access to the site (0 if never)
-                            lastaccess : Option[BigDecimal] = None, //last access to the site (0 if never)
-                            auth : Option[String] = None, //Auth plugins include manual, ldap, imap, etc
+
+                            firstaccess : Option[Int] = None, //first access to the site (0 if never)
+                            lastaccess : Option[Int] = None, //last access to the site (0 if never)
                             suspended : Option[Boolean] = None, //Suspend user account, either false to enable user login or true to disable it
                             confirmed : Option[Boolean] = None, //Active user: 1 if confirmed, 0 otherwise
-                            lang : Option[String] = None, //Language code such as "en", must exist on server
+
+                            lang : Option[String] = None , //Language code such as "en", must exist on server
                             calendartype : Option[String] = None, //Calendar type such as "gregorian", must exist on server
+
                             theme : Option[String] = None, //Theme name such as "standard", must exist on server
-                            timezone : Option[String] = None, //Timezone code such as Australia/Perth, or 99 for default
+
+                            timezone : String = "99", //Timezone code such as Australia/Perth, or 99 for default
+
                             mailformat : Option[Int] = None, //Mail format code is 0 for plain text, 1 for HTML etc
                             description : Option[String] = None, //User profile description
                             descriptionformat : Option[Int] = None, //int format (1 = HTML, 0 = MOODLE, 2 = PLAIN or 4 = MARKDOWN)
+
                             city : Option[String] = None, //Home city of the user
                             country : Option[String] = None, //Home country code of the user, such as AU or CZ
+
                             url : Option[String] = None, //URL of the user
                             profileimageurlsmall: Option[String] = None,   //User image profile URL - small version
                             profileimageurl : Option[String] = None,   //User image profile URL - big version
+
                             customfields: Option[List[UserCustomField]] = None, //User custom fields (also known as user profile fields)
-                            preferences: Option[List[UserPreference]] = None                          )
+                            preferences: Option[List[UserPreference]] = None
+                          )
