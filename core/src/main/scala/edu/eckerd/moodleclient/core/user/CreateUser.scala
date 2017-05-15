936cab6ebe1d1d8dacd387ab3d5922ddfe30bfd8
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-05-15 13:06:45 -0400

    Added Api For Single User Creation

--- core/src/main/scala/edu/eckerd/moodleclient/core/user/CreateUser.scala
+++ core/src/main/scala/edu/eckerd/moodleclient/core/user/CreateUser.scala
@@ -1,6 +1,16 @@
 package edu.eckerd.moodleclient.core.user
 
-import edu.eckerd.moodleclient.models.User
+import edu.eckerd.moodleclient.MoodleAble
+import edu.eckerd.moodleclient.models.{CreatedUser, User}
+import org.http4s.UrlForm
 
 case class CreateUser(user: User, password: Option[String] = Some("Default#123"), createPassword: Boolean = false)
 
+object CreateUser {
+
+  implicit val CreateUserMoodleAble = new MoodleAble[CreateUser, List[CreatedUser]]{
+    override def render(input: CreateUser): UrlForm =
+      CreateUsers.renderUsers(List(input))
+  }
+
+}
