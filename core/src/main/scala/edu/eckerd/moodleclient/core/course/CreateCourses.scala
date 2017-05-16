f8816dd42b2c186327ee73e0174fcb65d927012c
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-05-16 10:03:12 -0400

    Create Courses Enhancement

--- core/src/main/scala/edu/eckerd/moodleclient/core/course/CreateCourses.scala
+++ core/src/main/scala/edu/eckerd/moodleclient/core/course/CreateCourses.scala
@@ -1,5 +1,6 @@
 package edu.eckerd.moodleclient.core.course
 
+import cats.Show
 import edu.eckerd.moodleclient.MoodleAble
 import edu.eckerd.moodleclient.models.Course
 import io.circe.Json
@@ -12,65 +13,59 @@ object CreateCourses {
 
   implicit val CreateCourseMoodleAble: MoodleAble[CreateCourses, Json] = new MoodleAble[CreateCourses, Json] {
     def render(input: CreateCourses): UrlForm = {
-      val functionMap = Map(
-        "wsfunction" -> List("core_course_create_courses")
-      )
 
       UrlForm(
-        functionMap ++ createCoursesMap(input.courses)
+       renderCourses(input.courses):_*
       )
     }
   }
 
-  private def createCoursesMap(
-                        list: List[Course],
-                        start: Int = 0,
-                        acc: List[Map[String, Seq[String]]] = List.empty[Map[String, Seq[String]]]
-                      ): Map[String, Seq[String]] = {
-    list match {
-      case x :: xs =>
-        createCoursesMap(xs, start+1, createCourseMap(x, start) :: acc)
-      case Nil =>
-        acc.foldLeft(Map.empty[String, Seq[String]]) { (acc, m) =>acc ++ m}
+  def renderCourses(courses: List[Course]): List[(String, String)] = {
+    val function = ("wsfunction", "core_course_create_courses")
+    val renderedCourses = courses.zipWithIndex.flatMap{case (course, index) => renderCourse(course, index)}
+
+    function :: renderedCourses
+  }
+
+  def renderCourse(course: Course, index: Int): List[(String, String)] = {
+    def renderOptValue[A](call: String, value: Option[A])(implicit show: Show[A]): List[(String, String)] = {
+      value.map(a => List((call, a.show)))
+      .getOrElse(List.empty[(String, String)])
     }
 
-  }
-
-  private def createCourseMap(
-                               course: Course,
-                               arrayVal: Int
-                             ): Map[String, Seq[String]] = {
-
-    val permanent = Map(
-      s"courses[$arrayVal][fullname]" -> List(course.fullname),
-      s"courses[$arrayVal][shortname]" -> List(course.shortname)
+    val permanent = List(
+      (s"courses[$index][fullname]", course.fullname),
+      (s"courses[$index][shortname]", course.shortname)
     )
 
-    val category = course.categoryid.map( c => Map(s"courses[$arrayVal][categoryid]" -> List(c.show))).getOrElse(Map.empty[String, Seq[String]])
-    val idNumber = course.idnumber.map(id => Map(s"courses[$arrayVal][idnumber]" -> List(id.show))).getOrElse(Map.empty[String, Seq[String]])
-    permanent ++ category ++ idNumber
+    val categoryid = renderOptValue(s"courses[$index][categoryid]", course.categoryid)
+    val idNumber = renderOptValue(s"courses[$index][idnumber]" ,course.idnumber)
+    val summary = renderOptValue(s"courses[$index][idnumber]", course.summary)
+    val summaryFormat = renderOptValue(s"courses[$index][summaryformat]", course.summaryformat)
+    val format = renderOptValue(s"courses[$index][format]", course.format)
+    val showgrades = renderOptValue(s"courses[$index][showgrades]", course.showgrades)
+    val newsitems = renderOptValue(s"courses[$index][newsitems]", course.newsitems)
+    val startdate = renderOptValue(s"courses[$index][startdate]", course.startdate)
+    val enddate = renderOptValue(s"courses[$index][enddate]", course.enddate)
+    val numsections = renderOptValue(s"courses[$index][numsections]", course.numsections)
+    val maxbytes = renderOptValue(s"courses[$index][maxbytes]", course.maxbytes)
+    val showreports = renderOptValue(s"courses[$index][showreports]", course.showreports)
+    val visible = renderOptValue(s"courses[$index][visible]", course.visible)
+//    val hiddensections = renderOptValue(s"courses[$index][hiddensections]", course.hiddensections)
+    val groupmode = renderOptValue(s"courses[$index][groupmode]", course.groupmode)
+    val groupmodeforce = renderOptValue(s"courses[$index][groupmodeforce]", course.groupmodeforce)
+    val defaultgroupingid = renderOptValue(s"courses[$index][defaultgroupingid]", course.defaultgroupingid)
+    val enablecompletion = renderOptValue(s"courses[$index][enablecompletion]", course.enablecompletion)
+    val completionnotify = renderOptValue(s"courses[$index][completionnotify]", course.completionnotify)
+    val lang = renderOptValue(s"courses[$index][lang]", course.lang)
+    val forcetheme = renderOptValue(s"courses[$index][forcetheme]", course.forcetheme)
+
+
+    permanent ::: categoryid ::: idNumber ::: summary ::: summaryFormat ::: format ::: showgrades ::: newsitems :::
+    startdate ::: enddate ::: numsections ::: maxbytes ::: showreports ::: visible ::: groupmode ::: groupmodeforce :::
+    defaultgroupingid ::: enablecompletion ::: completionnotify ::: lang ::: forcetheme
   }
-//
-//    s"courses[$arrayVal][summary]" ->
-//    s"courses[$arrayVal][summaryformat]" ->
-//    s"courses[$arrayVal][format]" ->
-//    s"courses[$arrayVal][showgrades]" ->
-//    s"courses[$arrayVal][newsitems]" ->
-//    s"courses[$arrayVal][startdate]" ->
-//    s"courses[$arrayVal][enddate]" ->
-//    s"courses[$arrayVal][numsections]" ->
-//    s"courses[$arrayVal][maxbytes]" ->
-//    s"courses[$arrayVal][showreports]" ->
-//    s"courses[$arrayVal][visible]" ->
-//    s"courses[$arrayVal][hiddensections]" ->
-//    s"courses[$arrayVal][groupmode]" ->
-//    s"courses[$arrayVal][groupmodeforce]" ->
-//    s"courses[$arrayVal][defaultgroupingid]" ->
-//    s"courses[$arrayVal][enablecompletion]" ->
-//    s"courses[$arrayVal][completionnotify]" ->
-//    s"courses[$arrayVal][lang]" ->
-//    s"courses[$arrayVal][forcetheme]" ->
-//
+
 //    s"courses[$arrayVal][courseformatoptions][0][name]= string
 //    s"courses[$arrayVal][courseformatoptions][0][value]= string
 
