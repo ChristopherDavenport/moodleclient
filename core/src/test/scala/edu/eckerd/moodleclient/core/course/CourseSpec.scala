156fc2e21bdbc2e59d6addc9c9f9eb88375612a4
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-05-16 13:49:28 -0400

    Addition Course Get Tests

--- core/src/test/scala/edu/eckerd/moodleclient/core/course/CourseSpec.scala
+++ core/src/test/scala/edu/eckerd/moodleclient/core/course/CourseSpec.scala
@@ -3,6 +3,7 @@ package edu.eckerd.moodleclient.core.course
 import org.http4s.UrlForm
 import org.scalatest.{FlatSpec, Matchers}
 import GetCourses.GetCoursesMoodleAble
+import GetCategories.getCategoriesMoodleAble
 
 class CourseSpec extends FlatSpec with Matchers {
 
@@ -12,6 +13,21 @@ class CourseSpec extends FlatSpec with Matchers {
     }
 
     GetCoursesMoodleAble.render(GetCourses()) shouldBe expected
+  }
+
+  "GetCategoriesMoodleable" should "generate a UrlForm with the function name" in {
+    val expected = UrlForm(("wsfunction", "core_course_get_categories"))
+    getCategoriesMoodleAble.render(GetCategories()) shouldBe expected
+  }
+
+  "GetContentsMoodleAble" should "generate a urlform with the function name and id" in {
+    val id = "id"
+    val expected = UrlForm(
+      ("wsfunction", "core_course_get_contents"),
+      ("courseid", id)
+    )
+
+    GetContents.GetContentsMoodleAble.render(GetContents("id")) shouldBe expected
   }
 
 }
