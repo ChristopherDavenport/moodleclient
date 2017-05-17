d97d2be5c422fba7746e9301b729d1790c922fa4
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-05-17 11:47:17 -0400

    To Create A User, You Need to Have a user to create

--- core/src/main/scala/edu/eckerd/moodleclient/core/user/CreateUser.scala
+++ core/src/main/scala/edu/eckerd/moodleclient/core/user/CreateUser.scala
@@ -1,5 +1,6 @@
 package edu.eckerd.moodleclient.core.user
 
+import cats.data.NonEmptyList
 import edu.eckerd.moodleclient.MoodleAble
 import edu.eckerd.moodleclient.models.{CreatedUser, User}
 import org.http4s.UrlForm
@@ -10,7 +11,7 @@ object CreateUser {
 
   implicit val CreateUserMoodleAble = new MoodleAble[CreateUser, List[CreatedUser]]{
     override def render(input: CreateUser): UrlForm =
-      CreateUsers.renderUsers(List(input))
+      CreateUsers.renderUsers(NonEmptyList.of(input))
   }
 
 }
